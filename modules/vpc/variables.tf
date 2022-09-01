33b8126285ae25118ed0426b2b3a4d0901254ee2
TTwY1203 <hhyun1203@naver.com>
2022-09-01 16:23:37 +0900

    vpc moudle add main.tf, outputs.tf, variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "Desired name for the VPC resources."
+  type        = string
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a vaild CIDR(0.0.0.0/0), but not acceptable by AWS and should be oveeridden"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC. Only support 'default' or 'dedicated'."
+  type        = string
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block"
+  type        = bool
+  default     = false
+}
