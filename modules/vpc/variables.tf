cb42a8ec7233ce4464b63739e85aa55946a2f267
TTwY1203 <hhyun1203@naver.com>
2022-09-15 17:41:52 +0900

    add variables _dhcp_option, _vpn_gateway

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -10,6 +10,18 @@ variable "vpc_tags" {
   default     = {}
 }
 
+variable "igw_tags" {
+  description = "Additional tags for the internet gateway"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpn_gateway_tags" {
+  description = "Additional tags for the VPN gateway"
+  type        = map(string)
+  default     = {}
+}
+
 variable "name" {
   description = "Desired name for the VPC resources."
   type        = string
@@ -49,4 +61,64 @@ variable "secondary_cidr_blocks" {
   description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
   type        = list(string)
   default     = []
+}
+
+variable "internet_gateway_enabled" {
+  description = "Should be true if you want to create a new Internet Gateway resource and attach it to the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "egress_only_internet_gateway_enabled" {
+  description = "Should be true if you want to create a new Egress Only Internet Gateway resource and attach it to the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dhcp_options" {
+  description = "Should be true if you want to specify a DHCP options set with a custom domain name, DNS servers, NTP servers, netbios servers, and/or netbios server type"
+  type        = bool
+  default     = false
+}
+
+variable "dhcp_options_domain_name" {
+  description = "Specifies DNS name for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = string
+  default     = ""
+}
+
+variable "dhcp_options_domain_name_servers" {
+  description = "Specify a list of DNS server addresses for DHCP options set, default to AWS provided (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = ["AmazonProvidedDNS"]
+}
+
+variable "dhcp_options_ntp_servers" {
+  description = "Specify a list of NTP servers for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "dhcp_options_netbios_name_servers" {
+  description = "Specify a list of netbios servers for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "dhcp_options_netbios_node_type" {
+  description = "Specify netbios node_type for DHCP options set (requires enable_dhcp_options set to true)"
+  type        = list(string)
+  default     = []
+}
+
+variable "vpn_gateway_enabled" {
+  description = "Should be true if you want to create a new VPN Gateway resource and attach it to the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "amazon_side_asn" {
+  description = "The Autonomous System Number (ASN) for the Amazon side of the gateway. By default the virtual private gateway is created with the current default Amazon ASN."
+  type        = string
+  default     = "64512"
 }
