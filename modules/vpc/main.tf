9179d99c63868bb7720972ea5e08e5acb6d37f65
TTwY1203 <hhyun1203@naver.com>
2022-10-08 10:18:37 +0900

    modify aws_vpc.this.id

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_vpc_dhcp_options_association" "this" {
 resource "aws_internet_gateway" "this" {
   count = var.internet_gateway_enabled ? 1 : 0
 
-  vpc_id = local.vpc_id
+  vpc_id = aws_vpc.this.id
 
   tags = merge(
     { "Name" = var.name },
