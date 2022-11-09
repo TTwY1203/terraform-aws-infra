06e1b8c45f2cb106e82b20e4b942971a9c2ef8cc
TTwY1203 <hhyun1203@naver.com>
2022-11-09 14:24:32 +0900

    modify duplication

--- modules/nat-gateway/variables.tf
+++ modules/nat-gateway/variables.tf
@@ -38,12 +38,6 @@ variable "single_nat_gateway" {
   default     = false
 }
 
-variable "azs" {
-  description = "A list of availability zones names or ids in the region"
-  type        = list(string)
-  default     = []
-}
-
 variable "enable_nat_gateway" {
   description = "Should be true if you want to provision NAT Gateways for each of your private networks"
   type        = bool
